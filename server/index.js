e8e2572df768a23c1e2583c6b6f509b91e0129de
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2024-09-05 01:35:21 +0530

    .env set up

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const chats = require('./data/data');
 
 // Load environment variables from .env file
 dotenv.config();
@@ -10,6 +11,15 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('running');
+});
+
+app.get('/api/chats', (req,res)=>{
+    res.send(chats);
+})
+
+app.get('/api/chats/:id', (req, res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id); // Convert the string ID to a number
+    res.send(singleChat);
 });
 
 app.listen(PORT, () => {
