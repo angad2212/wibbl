2911f7cb5c2e947ae6ae327a4d820e285c01fcef
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2024-10-20 19:29:31 +0530

    null

--- client/src/Pages/Chat.jsx
+++ client/src/Pages/Chat.jsx
@@ -3,26 +3,9 @@ import axios from 'axios';
 
 const Chat = () => {
 
-    const [chats, setChats] = useState([]);
-
-  const fetchChats = async () => {
-    try {
-      const { data } = await axios.get('http://localhost:3000/api/chats');
-      setChats(data);  // This will log the chat data
-    } catch (error) {
-      console.error('Error fetching chats:', error);  // This will catch and log any errors
-    }
-  };
-
-  useEffect(() => {
-    fetchChats();
-  }, []);
-
   return (
     <div>
-      {chats.map((chat) => (
-        <div key={chat._id}>{chat.chatName}</div>  // Add a key prop for each list item
-      ))}
+      
     </div>
   );
 };
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -11,7 +11,11 @@ import {
 import Login from '../components/Authentication/Login'
 import Signup from '../components/Authentication/Signup'
 
+
 const Home = () => {
+
+
+ 
   return (
     <Container maxW="xl" centerContent>
       <Box
