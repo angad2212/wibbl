67ea2aba7e67979d041b8872b8187d14dbe4fd6f
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2024-11-05 12:24:12 +0530

    backend

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -9,7 +9,7 @@ router.route('/').get(protect, fetchChats);
 router.route('/group').post(protect, createGroupChat);
 router.route('/rename').put(protect, renameGroup);
 router.route('/groupadd').put(protect, addToGroup);
-router.route('groupremove').put(protect, removeFromGroup);
+router.route('/groupremove').put(protect, removeFromGroup);
 
 
 
