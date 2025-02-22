a59ff689182dcfe611a51fdf589206609705baf6
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2025-02-22 16:21:02 +0530

    installed jest

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,7 +5,7 @@ const {protect} =  require('../middleware/authMiddleware')
 const router = express.Router();
 
 router.route('/').post(registerUser).get(protect, allUsers); //first the protect middleware called
-//thsi middleware is used to very the token
+//this middleware is used to verify the token
 router.post('/login', authUser);
 
 module.exports = router
